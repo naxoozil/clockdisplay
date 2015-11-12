3e0c9a462b995e29ee787a7ec3ab06487722bdf6
Nacho <nachoviuda@gmail.com>
2015-11-12 09:12:19 +0100

    Añadidos cambios a metodos y añadios comentarios a metodos

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -8,50 +8,57 @@
 public class ClockDisplay
 {
     private int horas;
-    private int min;
+    private int minutos;
     /**
-     * Constructor for objects of class ClockDisplay
+     * Constructor for objects of the class ClockDisplay
      */
     public ClockDisplay()
     {
        horas = 00;
-       min = 00;
+       minutos = 00;
     }
 
     /**
-     * 
+     * Method to fix a time in the clock
      */
     
-    public void setTime(int hora, int minutos)
+    public void setTime(int hora, int minuto)
     {
-       if (hora < 24 && minutos < 60){
+       if (hora < 24 && minuto < 60){
            horas = hora;
-           min = minutos;
+           minutos = minuto;
         }
         else{
         System.out.println("Hora no admitida");
        }
     }
     
+    /**
+     * Method to sum a minute in the clock and change automatically
+     * the hour and the minutes
+     */
     public void timeTick()
     {
-       if (min >=59 && horas >= 23){
-            min = 00;
+       if (minutos >=59 && horas >= 23){
+            minutos = 00;
             horas = 00;
        }
-       else if (min >=59 && horas<= 23){
-            min = 00;
-            horas = horas+1;
+       else if (minutos >=59 && horas<= 23){
+            minutos = 00;
+            horas = horas + 1;
        }
        else {
-            min = min + 1;
+            minutos = minutos + 1;
        }
     }
     
+    /**
+     * Method to get the time of the clock
+     */
     public String getTime()
     {
         String HoraAc = "%02d:%02d";
-        String resultado = String.format(HoraAc,horas,min);
+        String resultado = String.format(HoraAc,horas,minutos);
         return resultado;
     }
 }
